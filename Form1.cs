094280f39b38a0e1dd252d7a3a3a5d0d03582498
zetung <v.s.kasperovich@gmail.com>
2021-03-02 00:07:12 +0300

    Laba1_OOP

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba1_OOP_Prototype1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Zagruzka(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+
+            Pen pen = new Pen(Color.Green);
+            graph.DrawLine(pen, 30, 50, 30, 200);
+
+            pictureBox1.Image = bmp;
+        }
+    }
+}
